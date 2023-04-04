f3d1374132d77e3ddf3f3e1352489d735b0406c5
EdwinMart2023 <123100388+EdwinMart2023@users.noreply.github.com>
2023-04-04 17:41:46 -0400

    Add files via upload
    the vscode

---
+++ movie_repository.py
@@ -0,0 +1,28 @@
+class MovieRepository:
+
+    def get_all_movies(self):
+
+        return db.session.query(Movie).all()
+        
+
+    def get_movie_by_id(self, movie_id):
+        def get_movie_by_id(self, movie_id):
+        return db.session.query(Movie).filter_by(id=movie_id).first()
+       
+
+    def create_movie(self, title, director, rating):
+        def create_movie(self, title, director, rating):
+    new_movie = Movie(title=title, director=director, rating=rating)
+    db.session.add(new_movie)
+    db.session.commit()
+        return new_movie
+  
+    
+
+    def search_movies(self, title):
+         return db.session.query(Movie).filter(Movie.title.ilike(f'%{title}%')).all()
+        
+
+
+# Singleton to be used in other modules
+movie_repository_singleton = MovieRepository()
