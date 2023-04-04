4f3aa22217d1cf236c987514c0e4f38f8ee15d3e
EdwinMart2023 <123100388+EdwinMart2023@users.noreply.github.com>
2023-04-04 17:31:26 -0400

    Update movie_repository.py
    added the TODO comments

--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -2,19 +2,22 @@ class MovieRepository:
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        return None
+        return db.session.query(Movie).all()
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
-        return None
+        return db.session.query(Movie).filter_by(id=movie_id).first()
 
     def create_movie(self, title, director, rating):
-        # TODO create a new movie in the DB
-        return None
+        def create_movie(self, title, director, rating):
+    new_movie = Movie(title=title, director=director, rating=rating)
+    db.session.add(new_movie)
+    db.session.commit()
+        return new_movie
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
+        return db.session.query(Movie).filter(Movie.title.ilike(f'%{title}%')).all()
 
 
 # Singleton to be used in other modules
